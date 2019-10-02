4829c8378c4de9ed137c0643d185b173a66bf874
Andrew Waller <andrewhwaller@gmail.com>
2019-10-02 00:18:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -82,18 +82,18 @@ const fi = (function() {
        receiver.push(val)
      },
 
-     flatten: function(collection, shallow, newArr=[]) {
-  if (!Array.isArray(collection)) return newArr.push(collection)
-  if (shallow) {
-    for (let val of collection)
-      Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val)
-  } else {
-    for (let val of collection) {
-      this.flatten(val, false, newArr)
-    }
-  }
-  return newArr
-},
+    flatten: function(collection, shallow, newArr=[]) {
+      if (!Array.isArray(collection)) return newArr.push(collection)
+      if (shallow) {
+        for (let val of collection)
+        Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val)
+      } else {
+        for (let val of collection) {
+          this.flatten(val, false, newArr)
+        }
+      }
+      return newArr
+    },
 
 
 
