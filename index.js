1174e8bc48617f4f24c9fd18e8804e465da0d046
Andrew Waller <andrewhwaller@gmail.com>
2019-10-02 00:21:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -139,21 +139,21 @@ const fi = (function() {
             values.push(obj[key])
           }
           return values
-    
+
           // Using the custom 'map' method from above
           // return this.map(obj, (value) => value)
-    
+
         },
-    
+
         functions: function(obj) {
           const functionNames = []
-    
+
           for (let key in obj) {
             if (typeof obj[key] === "function"){
               functionNames.push(key)
             }
           }
-    
+
           return functionNames.sort()
         },
 
